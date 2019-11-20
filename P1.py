7c3af64d5fb0779a87daf1db42952d65c24fbf38
Luis K <luis.koehler@pa-ats.com>
2019-11-20 12:15:20 +0100

    start

---
+++ P1.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.slider import Slider
+from kivy.uix.textinput import TextInput
+
+
+class StartScreen(GridLayout):
+
+    def __init__(self,**kwargs):
+        super(StartScreen,self).__init__(**kwargs)
+        self.cols = 3
+        self.add_widget(Label(text='',font_size='20sp'))
+        self.add_widget(Label(text='X-Value',font_size='20sp'))
+        self.add_widget(Label(text='Y-Value',font_size='20sp'))
+        self.add_widget(Label(text='Target Point',font_size='20sp'))
+        self.TP_X = TextInput(multiline=False,font_size='50sp')
+        self.add_widget(self.TP_X)
+        self.TP_Y= TextInput(multiline=False,font_size='50sp')
+        self.add_widget(self.TP_Y)
+        self.add_widget(Label(text='Landmark 1',font_size='20sp'))
+        self.LM1_X = TextInput(multiline=False,font_size='50sp')
+        self.add_widget(self.LM1_X)
+        self.LM1_Y= TextInput(multiline=False,font_size='50sp')
+        self.add_widget(self.LM1_Y)
+        self.add_widget(Label(text='Landmark 2',font_size='20sp'))
+        self.LM2_X = TextInput(multiline=False,font_size='50sp')
+        self.add_widget(self.LM2_X)
+        self.LM2_Y= TextInput(multiline=False,font_size='50sp')
+        self.add_widget(self.LM2_Y)
+        self.add_widget(Label(text='Landmark 3',font_size='20sp'))
+        self.LM3_X = TextInput(multiline=False,font_size='50sp')
+        self.add_widget(self.LM3_X)
+        self.LM3_Y= TextInput(multiline=False,font_size='50sp')
+        self.add_widget(self.LM3_Y)
+        self.add_widget(Label(text=''))
+        DrawButton = Button(text='Draw' ,font_size='60sp',background_color=[0,1,0,1])
+        DrawButton.bind(on_press = self.CalculateDrawing)
+        self.add_widget(myButton)
+        self.add_widget(Label(text=''))
+    
+    def CalculateDrawing(self):
+        X = np.arange(-10, 10, 1)
+        Y = np.arange(-10, 10, 1)
+        U, V = np.meshgrid(X, Y)
+        fig, ax = plt.subplots()
+        q = ax.quiver(X, Y, U, V)
+        ax.quiverkey(q, X=0.3, Y=1.1, U=10,
+                        label='Quiver key, length = 10', labelpos='E')
+        plt.show()
+
+class TestApp(App):
+    def build(self):
+        return StartScreen()
+
+TestApp().run()
+
