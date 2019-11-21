0c2daee37b8541677566683c74f36fdabc08ef80
Luis K <luis.koehler@pa-ats.com>
2019-11-21 17:10:38 +0100

    EOD klappt immernochnichts

--- P1.py
+++ P1.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sympy import *
+from sympy.geometry import *
+
+
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
@@ -28,7 +32,7 @@ class StartScreen(GridLayout):
         self.add_widget(Label(text='Landmark 1',font_size='20sp'))
         self.LM1_X = TextInput(multiline=False,font_size='50sp')
         self.add_widget(self.LM1_X)
-        self.LM1_Y= TextInput(multiline=False,font_size='50sp')
+        self.LM1_Y = TextInput(multiline=False,font_size='50sp')
         self.add_widget(self.LM1_Y)
 
         self.add_widget(Label(text='Landmark 2',font_size='20sp'))
@@ -50,36 +54,38 @@ class StartScreen(GridLayout):
         self.add_widget(Label(text=''))
     
     def CalculateDrawing(self,instance):
-        x,y = np.meshgrid(np.arange(-7, 7.25,0.25), np.arange(-7, 7.25,0.25))
 
-        u = x
-        v = y
+        LM1= Circle(Point(float(self.LM1_X.text),float(self.LM1_Y.text)),1)
+        LM2= Circle(Point(float(self.LM2_X.text),float(self.LM2_Y.text)),1)
+        LM3= Circle(Point(float(self.LM3_X.text),float(self.LM3_Y.text)),1)
 
-        self.QuiverPlot(x, y, u, v)
+        TPC = Circle((float(self.TP_X.text),float(self.TP_Y.text),TP.canberra_distance(LM1)) 
+        InPoint = TPC.intersection(LM1)
+        print(InPoint)
+        u, v = np.meshgrid(np.arange(-7, 8, 1), np.arange(-7, 8, 1))
+        self.QuiverPlot(u, v)
 
-    def QuiverPlot(self,x, y, u, v):
+    def QuiverPlot(self,u,v):
         # Create quiver figure
-        x_direct = [1,1,1]
-        y_direct = [1,1,1]
-
         fig, ax = plt.subplots()
         ax.axis([-7, 7, -7, 7])
-        
-        ax.quiver(X[::3, ::3], Y[::3, ::3],x_direct,angles='xy', scale_units='xy', scale=1)
+
+        x, y = np.meshgrid(np.arange(-7, 8, 1), np.arange(-7, 8, 1))
+
+        ax.quiver(x, y,u,v,angles='xy', scale_units='xy', scale=1)
 
         LMX=np.array([float(self.LM1_X.text), float(self.LM2_X.text) ,float(self.LM3_X.text)])
         LMY=np.array([float(self.LM1_Y.text),float(self.LM2_Y.text),float(self.LM3_Y.text)])
-
         ax.scatter(float(self.TP_X.text),float(self.TP_Y.text),color='r') #Honeypot
         ax.scatter(LMX,LMY,color='g') #Landmarks
 
         ax.set_title('Honey Way')
         plt.show()
-        exit()
 
 class TestApp(App):
     def build(self):
         return StartScreen()
+
 
 TestApp().run()
 
